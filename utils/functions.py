4d421be9678bce02791acbaa75e491a4fa0796d1
danil <shuvalov58@icloud.com>
2023-10-08 20:21:21 +0300

    add new functions

--- utils/functions.py
+++ utils/functions.py
@@ -11,7 +11,7 @@ def open_file(path):
 
     new_sort = []
     for i in range(len(open_files)):
-        if len(open_files[i]) > 0 and open_files[i]["state"] == "EXECUTED":
+        if len(open_files[i]) > 0 and open_files[i]["state"] == "EXECUTED" and 'Перевод' in open_files[i]['description']:
             new_sort.append(open_files[i])
     return new_sort
 
@@ -21,4 +21,36 @@ def sort_files(new_files):
     Сортировка файлов по дате и возвращение последних 5 операций
     """
     sorted_files = sorted(new_files, key=lambda x: x['date'], reverse=True)
+
     return sorted_files[:5]
+
+
+def form_list(sort_file):
+    for i in sort_file:
+        #date = i["date"].strftime("%d.%m.%Y")
+
+        # Маскируем номер карты отправителя
+        masked_card_number = mask_card_number(i["from"])
+
+        # Маскируем номер  карты получателя
+        masked_account_number = mask_account_number(i["to"])
+
+        # Выводим информацию о переводе
+        #print(f"{date} {sort_file['description']}")
+        print(f"{masked_card_number} -> Счет {masked_account_number}")
+        print(f"{i['operationAmount']['amount']} {i['operationAmount']['currency']['name']}")
+
+        # Разделяем операции пустой строкой
+        print()
+
+
+def mask_card_number(card_number):
+    # Видны первые 6 цифр и последние 4, разбитые по блокам по 4 цифры
+    name_card = (''.join([x for x in card_number if x.isalpha()]))
+    number_card = (''.join([x for x in card_number if x.isdigit()]))
+    return f"{name_card} {number_card[:4]} {number_card[4:6]}** **** {number_card[12:]}"
+
+
+def mask_account_number(card_number):
+    # Видны только последние 4 цифры номера счета
+    return f"**{card_number[-4:]}"
