a26e9807ef60a380578df5ba0766abe978a9d0f3
danil <shuvalov58@icloud.com>
2023-10-08 16:56:07 +0300

    update

--- utils/functions.py
+++ utils/functions.py
@@ -1,47 +1,28 @@
 import json
 
-operation = "operations.json"
-with open(operation, 'r', encoding='utf-8') as file:
-    data_load = json.load(file)
 
-def last_transactions(operation):
-
-
-    # Отсортируем операции по дате в обратном порядке
-    sorted_transactions = sorted(data_load, key=lambda t: t["date"], reverse=True)
-
-    # Выводим на экран последние 5 операций
-    for i in range(min(5, len(sorted_transactions))):
-        transaction = sorted_transactions[i]
-
-        # Форматируем дату перевода
-        date = data_load["date"].strftime("%d.%m.%Y")
-
-        # Маскируем номер карты
-        masked_card_number = mask_card_number(transaction["card_number"])
-
-        # Маскируем номер счета
-        masked_account_number = mask_account_number(transaction["account_number"])
-
-        # Выводим информацию о переводе
-        print(f"{date} {data_load['description']}")
-        print(f"{masked_card_number} -> {masked_account_number}")
-        print(f"{data_load['amount']} {data_load['currency']}")
-
-        # Разделяем операции пустой строкой
-        print()
+def open_file(path):
+    """
+    Открытие файла со всеми операциями
+    """
+    with open(path, 'r', encoding="utf-8") as file:
+        open_files = json.load(file)
+    return open_files
 
 
-def bank_cart(data_load):
-    return data_load(["from"]).isalpha
+def sort_operation(files):
+    """
+
+    """
+    new_sort = []
+    for i in range(len(files)):
+        if len(files[i]) > 0 and files[i]["state"] == "EXECUTED":
+            new_sort.append(files[i])
+    return new_sort
 
 
-def mask_card_number(data_load):
-    number = data_load(["from"]).isdigit
-    # Видны первые 6 цифр и последние 4, разбитые по блокам по 4 цифры
-    return " ".join([number[:4], number[4:8], number[8:12], number[12:]])
-
-
-def mask_account_number(data_load):
-    # Видны только последние 4 цифры номера счета
-    return f"**{data_load[id][-4:]}"
+def sort_files(new_files):
+    """
+    """
+    sorted_files = sorted(new_files, key=lambda x: x['date'], reverse=True)
+    return sorted_files
--- utils/main.py
+++ utils/main.py
@@ -1,2 +1,13 @@
-from utils.functions import mask_card_number
-from utils.functions import mask_account_number
+from utils.functions import open_file, sort_operation, sort_files
+
+
+def main():
+    files = open_file("operations.json")
+    new_files = sort_operation(files)
+    sort_file = sort_files(new_files)
+    return sort_file
+
+if __name__ == '__main__':
+    main()
+
+
