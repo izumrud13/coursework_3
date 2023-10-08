cdf79f76bc351b5d02c0c3c436f9693d2dd586ab
danil <shuvalov58@icloud.com>
2023-10-08 17:24:22 +0300

    new update code

--- utils/functions.py
+++ utils/functions.py
@@ -3,26 +3,22 @@ import json
 
 def open_file(path):
     """
-    Открытие файла со всеми операциями
+    Открытие файла со всеми операциями,
+    Создание корректного списка из выполненных операций и без пустых операций
     """
     with open(path, 'r', encoding="utf-8") as file:
         open_files = json.load(file)
-    return open_files
 
-
-def sort_operation(files):
-    """
-
-    """
     new_sort = []
-    for i in range(len(files)):
-        if len(files[i]) > 0 and files[i]["state"] == "EXECUTED":
-            new_sort.append(files[i])
+    for i in range(len(open_files)):
+        if len(open_files[i]) > 0 and open_files[i]["state"] == "EXECUTED":
+            new_sort.append(open_files[i])
     return new_sort
 
 
 def sort_files(new_files):
     """
+    Сортировка файлов по дате и возвращение последних 5 операций
     """
     sorted_files = sorted(new_files, key=lambda x: x['date'], reverse=True)
-    return sorted_files
+    return sorted_files[:5]
--- utils/main.py
+++ utils/main.py
@@ -1,11 +1,10 @@
-from utils.functions import open_file, sort_operation, sort_files
+from utils.functions import open_file, sort_files
 
 
 def main():
     files = open_file("operations.json")
-    new_files = sort_operation(files)
-    sort_file = sort_files(new_files)
-    return sort_file
+    sort_file = sort_files(files)
+    print(sort_file)
 
 if __name__ == '__main__':
     main()
